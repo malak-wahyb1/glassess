6cba69dfb33ff30facb367d451e661b49a5bdb66
Elalitareq <117711772+Elalitareq@users.noreply.github.com>
2023-06-21 02:08:23 +0300

    added node version to package .json

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
+import ProductInfo from "./productInfo.js";
 const productSchema = new mongoose.Schema(
   {
     type: {
